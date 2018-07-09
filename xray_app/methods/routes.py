3adc832cd5119e920b8dbdac57b58c3539033860
Kat Rowlinson <katherine.rowlinson@diamond.ac.uk>
2018-07-09 15:53:30 +0100

    Formatting

--- xray_app/methods/routes.py
+++ xray_app/methods/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, url_for
-#from xray_app import app
-from xray_app.methods.forms import xraylib_request, request_error #remember to add error and request classes
+#from xray_app import app - remove me?
+from xray_app.methods.forms import xraylib_request, request_error #remember to add request classes
 import xraylib
 
 methods = Blueprint('methods', __name__)
@@ -19,14 +19,14 @@ def validate_float(s):
                 return True
         except ValueError:
                 return False
-
-                    
+#------------------------------------------------------------------------------------------------------------
 @methods.route('/atomicweight', methods=['GET', 'POST'])
 def atomicweight():
         form = xraylib_request()
         if request.method == 'POST':
                 #for key in request.form.keys():
                 #        print(f'key= {key}')
+                
                 int_z = request.form['int_z']
                 
                 if validate_int(int_z) == False:    
@@ -49,27 +49,69 @@ def atomicweight():
                         )
                 
                 else:
-                        return render_template('atomicweight.html', title='Atomic Weight', form=form, int_z=int_z, error=request_error.int_z_error)                       
-        return render_template('atomicweight.html', title='Atomic Weight', form=form)
+                        return render_template(
+                        'atomicweight.html', 
+                        title='Atomic Weight', 
+                        form=form, 
+                        int_z=int_z, 
+                        error=request_error.int_z_error
+                        )                       
+        return render_template(
+        'atomicweight.html', 
+        title='Atomic Weight', 
+        form=form
+        )
         
-#---------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------
 @methods.route('/rayleigh_ff', methods=['GET', 'POST'])
 def rayleigh_form_factor():
         form = xraylib_request()
+        
         if request.method == 'POST':
-                #for key in request.form.keys():
-                #        print(f'key= {key}')
                 int_z = request.form['int_z']
                 float_q = request.form['float_q']
+                
                 if validate_int(int_z) == False:
-                        return render_template('rayleigh_ff.html', title='Rayleigh Form Factor', form=form, int_z=int_z, float_q=float_q, error=request_error.int_z_error)
-                #try def a render template function to make this neater!!        
+                        return render_template(
+                        'rayleigh_ff.html', 
+                        title='Rayleigh Form Factor', 
+                        form=form, 
+                        int_z=int_z, 
+                        float_q=float_q, 
+                        error=request_error.int_z_error
+                        )
+                       
                 elif validate_float(float_q) == False:
-                        return render_template('rayleigh_ff.html', title='Rayleigh Form Factor', form=form, int_z=int_z, float_q=float_q, error=request_error.float_q_error) 
+                        return render_template(
+                        'rayleigh_ff.html', 
+                        title='Rayleigh Form Factor', 
+                        form=form, 
+                        int_z=int_z, 
+                        float_q=float_q, 
+                        error=request_error.float_q_error
+                        ) 
                 elif 0<int(int_z)<=118:                
                         print(f'int_z: {int_z}')
                         rff = xraylib.FF_Rayl(int(int_z), float(float_q))
-                        return render_template('rayleigh_ff.html', title='Rayleigh Form Factor', form=form, int_z=int_z, float_q=float_q, function=rff)
+                        return render_template(
+                        'rayleigh_ff.html', 
+                        title='Rayleigh Form Factor', 
+                        form=form, 
+                        int_z=int_z, 
+                        float_q=float_q, 
+                        function=rff
+                        )
                 else:
-                        return render_template('rayleigh_ff.html', title='Rayleigh Form Factor', form=form, int_z=int_z, float_q=float_q, error=request_error.error)                       
-        return render_template('rayleigh_ff.html', title='Rayleigh Form Factor', form=form)  
+                        return render_template(
+                        'rayleigh_ff.html', 
+                        title='Rayleigh Form Factor', 
+                        form=form, 
+                        int_z=int_z, 
+                        float_q=float_q, 
+                        error=request_error.error
+                        )                       
+        return render_template(
+        'rayleigh_ff.html', 
+        title='Rayleigh Form Factor', 
+        form=form
+        )  
