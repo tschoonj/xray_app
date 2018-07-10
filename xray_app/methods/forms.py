f51013995821f8da444dd9fba0393fec5f1ce165
Kat Rowlinson <katherine.rowlinson@diamond.ac.uk>
2018-07-10 15:14:58 +0100

    Dropdown filter attempt

--- xray_app/methods/forms.py
+++ xray_app/methods/forms.py
@@ -8,10 +8,12 @@ class Xraylib_Request(FlaskForm):
     validators=[DataRequired(message="Invalid response"), 
     NumberRange(min=1, max=118)]
     )
+    
     float_q = StringField(
     'Momentum Transfer', 
-    validators=[DataRequired(message="Invalid response")])  
-  
+    validators=[DataRequired(message="Invalid response")]
+    )
+          
 class Request_Error():
         int_z_error = 'Invalid input: please enter an integer.'
         float_q_error = 'Invalid input : please enter an number.'
