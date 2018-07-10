13a0387cbe70f1cb4356e87d4338d824b5c0a5c1
Kat Rowlinson <katherine.rowlinson@diamond.ac.uk>
2018-07-10 14:04:03 +0100

    Fix navbar. Add dropdown.

--- xray_app/main/routes.py
+++ xray_app/main/routes.py
@@ -8,7 +8,7 @@ def index():
         
 @main.route("/about")
 def about():
-        return render_template('about.html', title = 'About')
+        return render_template('about.html', title = 'About Us:')
         
 @main.route("/plot")
 def plot():
