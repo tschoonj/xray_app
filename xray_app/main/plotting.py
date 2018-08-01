2a731c38b99deff19d7765a20dac0c78c02ac085
Kat Rowlinson <katherine.rowlinson@diamond.ac.uk>
2018-08-01 09:18:04 +0100

    Graphs are made/shown

--- xray_app/main/plotting.py
+++ xray_app/main/plotting.py
@@ -1,42 +1,18 @@
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-import xraylib 
-fig = Figure() #Figure is the final image which may contain 1 or more Axes. 
 
-# A canvas must be manually attached to the figure (pyplot would automatically
-# do it).  This is done by instantiating the canvas with the figure as
-# argument.
-FigureCanvas(fig)
-
-# Axes represent an individual plot (don’t confuse this with the word “axis”, which refers to the x/y axis of a plot)
-
-x = []
-y = []
-
-def graph_data():   
-    for i in range(1, 20, 1):
-        x.append(i)
-        y.append(xraylib.ElementDensity(i)) #change to elementdensity
-
-def print_data(*lsts):
-    lst = zip(*lsts)    
-    for value in lst:
-        print(value)
     
-graph_data()
-print_data(x, y)
-
 from matplotlib import rcParams
 rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Tahoma']
 rcParams.update({'figure.autolayout': True})
-import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
-ax.plot(x, y, label='ElementDensity')
-ax.set(xlabel='Element', ylabel='Density')
 
-ax.legend()
-fig.savefig('elementdensity.png', dpi=80)
+def make_fig(x, y):
+    fig = Figure()
+    FigureCanvas(fig)
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label='ElementDensity')
+    ax.set(xlabel='Element', ylabel='Density')
+    ax.legend()
+    fig.savefig('elementdensity.png', dpi=80)
 
-plt.show()
+    plt.show()
--- xray_app/main/routes.py
+++ xray_app/main/routes.py
@@ -1,34 +1,114 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, make_response
 from xray_app.main.forms import Xraylib_Request_Plot
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import xraylib
+
 from xray_app.methods.routes import cs_tup
 
 main = Blueprint('main', __name__)
 
 @main.route("/about")
 def about():
-        return render_template('about.html', title = 'About ')
-        
+    return render_template('about.html', title = 'About ')
+
+@main.route("/plot/fig.png")
+def fig():
+    form = Xraylib_Request_Plot()
+    form.function.choices = form.function.choices + cs_tup
+    import matplotlib.pyplot as plt
+    from io import BytesIO
+    
+    x = []
+    y = []
+
+    def graph_data():   
+        for i in range(1, 20, 1):
+            x.append(i)
+            y.append(xraylib.ElementDensity(i)) #change to elementdensity
+
+    def print_data(*lsts):
+        lst = zip(*lsts)    
+        for value in lst:
+            print(value)
+            
+    graph_data()
+    fig = Figure()
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label='Fig 1')
+    ax.set(xlabel='X', ylabel='Y')
+    ax.legend()
+    
+    canvas = FigureCanvas(fig)
+    fig.savefig('fig.png', dpi=80)
+    
+    png_output = BytesIO()
+    print(png_output.getvalue())
+    print(canvas.print_png)
+    canvas.print_png(png_output)
+    #return png_output.getvalue()
+    #canvas.print_png(png_output.getvalue()) #THIS IS THE PROBLEM
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response
+
 @main.route("/plot")
 def plot():
-        form = Xraylib_Request_Plot()
-        form.function.choices = form.function.choices + cs_tup
-        if request.method == 'POST':        
-               #for key in request.form.keys():
-                   #print(f'key= {key}')
+    form = Xraylib_Request_Plot()
+    form.function.choices = form.function.choices + cs_tup
+    """import matplotlib.pyplot as plt
+    from io import BytesIO
+    
+    x = []
+    y = []
+
+    def graph_data():   
+        for i in range(1, 20, 1):
+            x.append(i)
+            y.append(xraylib.ElementDensity(i)) #change to elementdensity
+
+    def print_data(*lsts):
+        lst = zip(*lsts)    
+        for value in lst:
+            print(value)
+            
+    graph_data()
+    fig = Figure()
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label='Fig 1')
+    ax.set(xlabel='X', ylabel='Y')
+    ax.legend()
+    
+    canvas = FigureCanvas(fig)
+    fig.savefig('fig.png', dpi=80)
+    
+    png_output = BytesIO()
+    print(png_output.getvalue())
+    print(canvas.print_png)
+    canvas.print_png(png_output)
+    #return png_output.getvalue()
+    #canvas.print_png(png_output.getvalue()) #THIS IS THE PROBLEM
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+    return response   """   
+    
+    if request.method == 'POST':        
+        #for key in request.form.keys():
+        #print(f'key= {key}')
                 
-                select_input = request.form.get('function')
-                int_z = request.form['int_z']
-                float_q = request.form['float_q']
-                comp = request.form['comp']
-                int_z_or_comp = request.form['int_z_or_comp']
-                energy = request.form['energy']
-                theta = request.form['theta']
-                phi = request.form['phi']
-                density = request.form['density']
-                pz = request.form['pz']
+        select_input = request.form.get('function')
+        int_z = request.form['int_z']
+        float_q = request.form['float_q']
+        comp = request.form['comp']
+        int_z_or_comp = request.form['int_z_or_comp']
+        energy = request.form['energy']
+        theta = request.form['theta']
+        phi = request.form['phi']
+        density = request.form['density']
+        pz = request.form['pz']
                 
-                return render_template('plot.html', title = 'Plot')
+        return render_template('plot.html', form = form, title = 'Plot', figure = fig.png)
                 
-        return render_template('plot.html', title = 'Plot')
+    return render_template('plot.html', title = 'Plot', form = form)
   
   
